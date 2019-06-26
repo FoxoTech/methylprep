797302e4c67d50e19657ebc066d3839c6fab1928
Marc Maxmeister <mmaxmeister@lifeegx.com>
2019-06-26 15:02:49 -0600

    bumped version; added authors

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='methpype',
-    version='0.1.0',
+    version='1.0.0',
     description='Python-based Illumina methylation array preprocessing software',
     long_description='Python-based Illumina methylation array preprocessing software',
     url='https://github.com/LifeEGX/methpype',
     license='MIT',
+    author='Life Epigenetics',
+    author_email='info@lifeegx.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
