6f192583c2c7cca62fd9bdfe7998e974a72f5fb7
Marc Maxmeister <mmaxmeister@lifeegx.com>
2019-07-22 15:35:50 -0600

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='methpype',
-    version='1.0.1',
+    version='1.0.2',
     description='Python-based Illumina methylation array preprocessing software',
     long_description='Python-based Illumina methylation array preprocessing software',
     url='https://github.com/LifeEGX/methpype',
