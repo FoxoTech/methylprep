27e10a9fba9ee6f32c06d202abaceb3f5b346074
Marc Maxmeister <mmaxmeister@lifeegx.com>
2019-08-21 16:17:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='methpype',
-    version='1.0.5',
+    version='1.0.6',
     description='Python-based Illumina methylation array preprocessing software',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
