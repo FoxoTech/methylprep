3ce41d782902d79e1163ea13af23ebd5db459a63
Marc Maxmeister <mmaxmeister@lifeegx.com>
2019-09-05 10:06:13 -0600

    dropped redundant tests and sped up one

--- tests/processing/test_pipeline_batch_size.py
+++ tests/processing/test_pipeline_batch_size.py
@@ -5,37 +5,38 @@ import numpy as np
 
 class TestBatchSize():
 
-    @staticmethod
-    def test_no_batch_size():
-        test_data_dir = 'docs/example_data/GSE69852'
-        df = pipeline.run_pipeline(test_data_dir, export=True)
-        if not np.isclose(df[0].unmethylated.data_frame.iloc[0]['noob'], 4119.633578946326):
-            raise AssertionError()
-        if not Path('docs/example_data/GSE69852/9247377093/9247377093_R02C01_processed.csv').is_file():
-            raise AssertionError()
+    # covered by other tests
+    #@staticmethod
+    #def test_no_batch_size():
+    #    test_data_dir = 'docs/example_data/GSE69852'
+    #    df = pipeline.run_pipeline(test_data_dir, export=True)
+    #    if not np.isclose(df[0].unmethylated.data_frame.iloc[0]['noob'], 4119.633578946326):
+    #        raise AssertionError()
+    #    if not Path('docs/example_data/GSE69852/9247377093/9247377093_R02C01_processed.csv').is_file():
+    #        raise AssertionError()
 
-    @staticmethod
-    def test_no_batch_size_betas():
-        test_data_dir = 'docs/example_data/GSE69852'
-        betas = pipeline.run_pipeline(test_data_dir, betas=True)
-        if not np.isclose(betas.iloc[0]['9247377093_R02C01'], 0.23623395577166542):
-            raise AssertionError()
-        if not Path('beta_values.pkl').is_file():
-            raise AssertionError()
+    #@staticmethod
+    #def test_no_batch_size_betas():
+    #    test_data_dir = 'docs/example_data/GSE69852'
+    #    betas = pipeline.run_pipeline(test_data_dir, betas=True)
+    #    if not np.isclose(betas.iloc[0]['9247377093_R02C01'], 0.23623395577166542):
+    #        raise AssertionError()
+    #    if not Path('beta_values.pkl').is_file():
+    #        raise AssertionError()
 
-    @staticmethod
-    def test_no_batch_size_m_values():
-        test_data_dir = 'docs/example_data/GSE69852'
-        m_values = pipeline.run_pipeline(test_data_dir, m_value=True)
-        if not np.isclose(m_values.iloc[0]['9247377093_R02C01'], -1.6575579144514734):
-            raise AssertionError()
-        if not Path('m_values.pkl').is_file():
-            raise AssertionError()
+    #@staticmethod
+    #def test_no_batch_size_m_values():
+    #    test_data_dir = 'docs/example_data/GSE69852'
+    #    m_values = pipeline.run_pipeline(test_data_dir, m_value=True)
+    #    if not np.isclose(m_values.iloc[0]['9247377093_R02C01'], -1.6575579144514734):
+    #        raise AssertionError()
+    #    if not Path('m_values.pkl').is_file():
+    #        raise AssertionError()
 
     @staticmethod
     def test_with_batch_size():
         test_data_dir = 'docs/example_data/GSE69852'
-        df = pipeline.run_pipeline(test_data_dir, export=True, batch_size=1)
+        df = pipeline.run_pipeline(test_data_dir, export=True, batch_size=1, sample_name='AdultLiver1')
         if not np.isclose(df[0].unmethylated.data_frame.iloc[0]['noob'], 4119.633578946326):
             raise AssertionError()
         if not Path('docs/example_data/GSE69852/9247377093/9247377093_R02C01_processed.csv').is_file():
