4c117939d3371f9e1a1a92aa9bef2181dc14aad8
marcmaxson <marcmaxmeister@gmail.com>
2021-04-09 10:37:08 -0600

    changes to prevent importlib bugs on unused code from breaking tests

--- methylprep/models/sketchy_probes.py
+++ methylprep/models/sketchy_probes.py
@@ -2,17 +2,18 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-# used to warn user if running a Catalog_ID that contains sketchy illumina probes
-from importlib import resources # py3.7+
-pkg_namespace = 'methylprep.models'
-
-with resources.path(pkg_namespace, 'illumina_sketchy_probes_996.npy') as probe_filepath:
-    sketchy_probes = np.load(probe_filepath)
-# "If the first 8 numbers of Sentrix_ID (i.e. xxxxxxxx0001) are greater or equal to 20422033,
-# then the BeadChip originates from production batches using the new manufacturing process."
-new_manufacturing_cutoff_id = 20422033
-
 def sketchy_probes_warning(filepath):
+    """ not implemented anywhere yet """
+    # used to warn user if running a Catalog_ID that contains sketchy illumina probes
+    from importlib import resources # py3.7+
+    pkg_namespace = 'methylprep.models'
+
+    with resources.path(pkg_namespace, 'illumina_sketchy_probes_996.npy') as probe_filepath:
+        sketchy_probes = np.load(probe_filepath)
+    # "If the first 8 numbers of Sentrix_ID (i.e. xxxxxxxx0001) are greater or equal to 20422033,
+    # then the BeadChip originates from production batches using the new manufacturing process."
+    new_manufacturing_cutoff_id = 20422033
+
     if Path(filepath).name.startswith('GSM'):
         catalog_id = int(Path(filepath).name.split('_')[1][:8])
     else:
